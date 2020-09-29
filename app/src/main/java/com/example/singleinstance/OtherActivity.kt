0d36ba03ddb23523ddbf40bb912a65f1d7d8a289
xiayiye5 <13343401268@qq.com>
2020-09-29 14:07:23 +0800

    Kotlin语法示例

--- app/src/main/java/com/example/singleinstance/OtherActivity.kt
+++ app/src/main/java/com/example/singleinstance/OtherActivity.kt
@@ -2,10 +2,22 @@ package com.example.singleinstance
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+        vtThird.run {
+            text = "三江集团"
+            setTextColor(ContextCompat.getColor(this@OtherActivity, R.color.colorAccent))
+            textSize = 18.0f
+        }
+        vtThird.apply {
+            text = "三江集团"
+            setTextColor(ContextCompat.getColor(this@OtherActivity, R.color.colorAccent))
+            textSize = 18.0f
+        }
     }
 }
