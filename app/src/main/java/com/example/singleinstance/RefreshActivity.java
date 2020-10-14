2aafe81974cb09b3581464487802671de24238dd
xiayiye5 <13343401268@qq.com>
2020-10-14 16:16:42 +0800

    增加代码获取SHA1值的方法

--- app/src/main/java/com/example/singleinstance/RefreshActivity.java
+++ app/src/main/java/com/example/singleinstance/RefreshActivity.java
@@ -1,17 +1,24 @@
 package com.example.singleinstance;
 
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * @author xiayiye5
@@ -31,10 +38,16 @@ public class RefreshActivity extends Activity implements RefreshListView.OnRefre
         setContentView(R.layout.activity_refresh);
         //[1]找到我们关心的控件
         refreshListView = (RefreshListView) findViewById(R.id.refreshListView1);
-
+        refreshListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //获取SHA1
+                Toast.makeText(RefreshActivity.this, getSHA1(), Toast.LENGTH_SHORT).show();
+            }
+        });
         dataLists = new ArrayList<String>();
         for (int i = 0; i < 25; i++) {
-            dataLists.add("这是一条微博数据" + i);
+            dataLists.add("点击获取SHA1值" + i);
 
         }
         //[3]设置数据适配器
@@ -45,6 +58,33 @@ public class RefreshActivity extends Activity implements RefreshListView.OnRefre
         //[5]设置加载更多数据
         refreshListView.setOnLoadingMoreListener(this);
 
+    }
+
+    private String getSHA1() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    getPackageName(), PackageManager.GET_SIGNATURES);
+            byte[] cert = info.signatures[0].toByteArray();
+            MessageDigest md = MessageDigest.getInstance("SHA1");
+            byte[] publicKey = md.digest(cert);
+            StringBuilder hexString = new StringBuilder();
+            for (int i = 0; i < publicKey.length; i++) {
+                String appendString = Integer.toHexString(0xFF & publicKey[i])
+                        .toUpperCase(Locale.US);
+                if (appendString.length() == 1) {
+                    hexString.append("0");
+                }
+                hexString.append(appendString);
+                hexString.append(":");
+            }
+            String result = hexString.toString();
+            return result.substring(0, result.length() - 1);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 
